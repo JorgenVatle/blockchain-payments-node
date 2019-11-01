c31a21bb52a1e59617c155434347d66210d32598
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-01 18:44:11 +0200

    Document BalanceUpdates request interface.

--- src/Interfaces/BlockchainApi.ts
+++ src/Interfaces/BlockchainApi.ts
@@ -1,6 +1,13 @@
 export declare namespace BlockchainApi {
+    interface ApiRequest {
+        /**
+         * Your Blockchain.info API key.
+         */
+        key: string;
+    }
+
     namespace GenerateAddress {
-        interface Request {
+        interface Request extends ApiRequest {
 
             /**
              * Your HD wallet xPub.
@@ -11,11 +18,6 @@ export declare namespace BlockchainApi {
              * Callback URL to receive payment updates on.
              */
             callback: string;
-
-            /**
-             * Your Blockchain.info API key.
-             */
-            key: string;
 
         }
 
@@ -35,6 +37,41 @@ export declare namespace BlockchainApi {
              * The payments callback URL you submitted with your address generation request.
              */
             callback: string;
+
+        }
+    }
+
+    namespace BalanceUpdates {
+        interface Request extends ApiRequest {
+
+            /**
+             * Bitcoin address you would like to monitor.
+             */
+            addr: string;
+
+            /**
+             * URL to receive payment notifications on.
+             */
+            callback: string;
+
+            /**
+             * Request notification behaviour.
+             * Likely whether to keep or end notifying the provided callback URL once the transaction reaches the
+             * specified number of confirmations.
+             */
+            onNotification: 'KEEP' | 'DELETE';
+
+            /**
+             * Number of confirmations to wait for before sending a notification to your callback URL.
+             * (Default: 3)
+             */
+            confs: number;
+
+            /**
+             * Address operation you would like to receive notifications for.
+             * (Default: 'ALL')
+             */
+            op: 'ALL' | 'RECEIVE' | 'SPEND'
 
         }
     }
