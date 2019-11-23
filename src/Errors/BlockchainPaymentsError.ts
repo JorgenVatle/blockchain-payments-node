068691ccb874625e9c81632e67dc0bee9b454b5a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-11-23 19:23:17 +0200

    Use message from API if available in BlockchainPaymentsError.

--- src/Errors/BlockchainPaymentsError.ts
+++ src/Errors/BlockchainPaymentsError.ts
@@ -9,7 +9,12 @@ export class BlockchainPaymentsError extends Error {
      * Blockchain Payments Error constructor.
      */
     constructor(message: string, data?: any) {
-        super(message);
+        if (data && data.message) {
+            super(data.message);
+        } else {
+            super(message);
+        }
+
         if (data) {
             this.data = data;
         }
